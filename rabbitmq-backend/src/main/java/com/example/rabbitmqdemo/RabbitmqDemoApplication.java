b5f51bfb8066bb405d2c15dc81bb8cdb0a77ffd4
bharath <95297353+bharath839@users.noreply.github.com>
2025-08-24 22:06:41 +0530

    Update RabbitmqDemoApplication.java

--- rabbitmq-backend/src/main/java/com/example/rabbitmqdemo/RabbitmqDemoApplication.java
+++ rabbitmq-backend/src/main/java/com/example/rabbitmqdemo/RabbitmqDemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+    @EnableEurekaClient
 public class RabbitmqDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(RabbitmqDemoApplication.class, args);
